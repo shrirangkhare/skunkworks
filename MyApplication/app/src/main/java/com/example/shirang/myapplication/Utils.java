ab700bc1247d3b76a1aff62884970103bf6fdfd0
Shrirang <shrirang.khare@gmail.com>
2016-01-06 21:17:55 +0530

    test

--- MyApplication/app/src/main/java/com/example/shirang/myapplication/Utils.java
+++ MyApplication/app/src/main/java/com/example/shirang/myapplication/Utils.java
@@ -4,6 +4,7 @@ package com.example.shirang.myapplication;
  * Created by shrirang on 5/1/16.
  */
 public class Utils {
+    // one change
     public static String truncate(String in, int num) {
         String upToNCharacters = in.substring(0, Math.min(in.length(), num));
         return upToNCharacters;
